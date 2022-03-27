292aaaed35610f03e60631b9a0ed53868f6c81c1
lirineu <lucasirineu@uol.com.br>
2022-03-27 01:06:42 -0300

    aprimorando strat

--- src/attacker.c
+++ src/attacker.c
@@ -16,46 +16,73 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+int modulo (int x){
+  if(x<0) return x * -1;
+  return x;
+}
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   {
     static int Rodada = 0;
-    int Max, Rand, DirVer, DirHor;
+    static position_t Spy;
+    static int Random;
+    int Max, Rand;
     direction_t Dir;
+
+    if(Rodada == 0){
+      Random = random()%10;
+    }
 
     Rodada++;
 
     Max = RAND_MAX;
-    Rand = random();
 
-    if(Rand > 0.7 * Max){
-        DirVer = -1;
-    }
-    else if(Rand < 0.3 * Max){
-        DirVer = 1;
-    }
-    else{
-        DirVer = 0;
+    if(Rodada == Random){
+      Spy = get_spy_position(defender_spy);
     }
 
-    Rand = random();
-
-
-    if(Rand > 0.9 * Max){
-        DirHor = -1;
-    }
-    else if(Rand < 0.8 * Max){
-        DirHor = 1;
+    if(Rodada>= Random){
+      if(modulo((int) Spy.i - attacker_position.i) > modulo((int) Spy.j - attacker_position.j)){
+         Dir.i = 0;
+         Dir.j = 1;
+      }
+      else if(Spy.i < attacker_position.i){
+        Dir.i = 1;
+        Dir.j = 0;
+      }
+      else{
+        Dir.i = -1;
+        Dir.j = 0;
+      }
     }
     else{
-        DirHor = 0;
+      Rand = random();
+
+      if(Rand > 0.7 * Max){
+          Dir.i = -1;
+      }
+      else if(Rand < 0.3 * Max){
+          Dir.i = 1;
+      }
+      else{
+          Dir.i = 0;
+      }
+
+      Rand = random();
+
+
+      if(Rand > 0.9 * Max){
+          Dir.j = -1;
+      }
+      else if(Rand < 0.8 * Max){
+          Dir.j = 1;
+      }
+      else{
+          Dir.j = 0;
+      }
     }
-
-    Dir = {DirVer, DirHor};
-
-    move_position(attacker_position, Dir);
-
-    return;    
+    return Dir;    
 }
 }
 
