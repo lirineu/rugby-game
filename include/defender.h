91cefb2b6b22abcf907c970e53d3d2a3b02bd0df
lirineu <lucasirineu@uol.com.br>
2022-03-27 00:33:52 -0300

    detalhes

--- include/defender.h
+++ include/defender.h
@@ -17,57 +17,7 @@
  */
 direction_t execute_defender_strategy(position_t defender_position, Spy attacker_spy){
 
-  static int Rodada = 0;
-  static position_t Spy;
-  direction_t Dir;
-  static int Random;
   
-  if(Rodada == 0){
-      Random = random()%10;
-  }
-
-  Rodada++;
-
-  if(Rodada < Random){
-    Dir.i = 0;
-    Dir.j = -1;
-  }
-  else if(Rodada == Random){
-    Spy = get_spy_position(attacker_spy);
-    if(Spy.j > defender_position.j){
-        Dir.i = Dir.j = 0;
-    }
-    else if(Spy.i > defender_position.i){
-        Dir.i = 1;
-        Dir.j = -1;
-    }
-    else if(Spy.i < defender_position.i){
-        Dir.i = Dir.j = -1;
-    }
-    else{
-        Dir.i = 0;
-        Dir.j = -1;
-    }
-
-  }
-  else{
-    if(Spy.j > defender_position.j){
-        Dir.i = Dir.j = 0;
-    }
-    else if(Spy.i > defender_position.i){
-        Dir.i = 1;
-        Dir.j = -1;
-    }
-    else if(Spy.i < defender_position.i){
-        Dir.i = Dir.j = -1;
-    }
-    else{
-        Dir.i = 0;
-        Dir.j = -1;
-    }
-  }
-  
-  return Dir;
 }
 
 #endif // DEFENDER_H
