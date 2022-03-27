18156b2271b61174e00ce6da46fdb5958c124787
lirineu <lucasirineu@uol.com.br>
2022-03-27 00:45:27 -0300

    i'm dumb

--- src/attacker.c
+++ src/attacker.c
@@ -18,12 +18,45 @@
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+  {
+    static int Rodada = 0;
+    int Max, Rand, DirVer, DirHor;
+    direction_t Dir;
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+    Rodada++;
+
+    Max = RAND_MAX;
+    Rand = random();
+
+    if(Rand > 0.7 * Max){
+        DirVer = -1;
+    }
+    else if(Rand < 0.3 * Max){
+        DirVer = 1;
+    }
+    else{
+        DirVer = 0;
+    }
+
+    Rand = random();
+
+
+    if(Rand > 0.9 * Max){
+        DirHor = -1;
+    }
+    else if(Rand < 0.8 * Max){
+        DirHor = 1;
+    }
+    else{
+        DirHor = 0;
+    }
+
+    Dir = {DirVer, DirHor};
+
+    move_position(attacker_position, Dir);
+
+    return;    
+}
 }
 
 /*----------------------------------------------------------------------------*/
