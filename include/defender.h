09d73ce3ab39a520a4971b5f446a5d17116d6045
lirineu <lucasirineu@uol.com.br>
2022-03-26 23:59:56 -0300

    Seus inimigos não conseguirão prever o que voce faz se nem voce sabe o que esta fazendo, sun tsu a arte da guerra

--- include/defender.h
+++ include/defender.h
@@ -1,6 +1,9 @@
 #ifndef DEFENDER_H
 #define DEFENDER_H
 
+//Standard headers
+#include <stdlib.h>
+
 // Internal headers
 #include "position.h"
 #include "spy.h"
@@ -13,6 +16,52 @@
  * they will take in the field.
  */
 direction_t execute_defender_strategy(position_t defender_position,
-                                      Spy attacker_spy);
+                                      Spy attacker_spy){
+  static int Rodada = 0;
+  static int Random = random();
+  static position_t Spy;
+  position_t Dir;
+  
+  Rodada++;
+  
+
+  if(Rodada < Random){
+    Dir = {0, -1};
+  }
+  else if(Rodada == Random){
+    Spy = get_spy_position(attacker_spy);
+    if(Spy.j > defender_position.j){
+        Dir = {0, 0};
+    }
+    else if(Spy.i > defender_position.i){
+        Dir = {1, -1};
+    }
+    else if(Spy.i < defender_position.i){
+        Dir = {-1, -1};
+    }
+    else{
+        Dir = {0, -1}
+    }
+
+  }
+  else{
+    if(Spy.j > defender_position.j){
+        Dir = {0, 0};
+    }
+    else if(Spy.i > defender_position.i){
+        Dir = {1, -1};
+    }
+    else if(Spy.i < defender_position.i){
+        Dir = {-1, -1};
+    }
+    else{
+        Dir = {0, -1}
+    }
+  }
+  
+  move_position(defender_position, Dir);
+
+  return;
+}
 
 #endif // DEFENDER_H
