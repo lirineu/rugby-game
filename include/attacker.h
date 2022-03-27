0b4291010bc68ff04191cb4fbeaaa9bbd6dbd276
lirineu <lucasirineu@uol.com.br>
2022-03-27 00:44:38 -0300

    i'm dumb

--- include/attacker.h
+++ include/attacker.h
@@ -16,44 +16,6 @@
  * they will take in the field.
  */
 direction_t execute_attacker_strategy(position_t attacker_position,
-                                      Spy defender_spy){
-    static int Rodada = 0;
-    int Max, Rand, DirVer, DirHor;
-    direction_t Dir;
-
-    Rodada++;
-
-    Max = RAND_MAX;
-    Rand = random();
-
-    if(Rand > 0.7 * Max){
-        DirVer = -1;
-    }
-    else if(Rand < 0.3 * Max){
-        DirVer = 1;
-    }
-    else{
-        DirVer = 0;
-    }
-
-    Rand = random();
-
-
-    if(Rand > 0.9 * Max){
-        DirHor = -1;
-    }
-    else if(Rand < 0.8 * Max){
-        DirHor = 1;
-    }
-    else{
-        DirHor = 0;
-    }
-
-    Dir = {DirVer, DirHor};
-
-    move_position(attacker_position, Dir);
-
-    return;    
-}
+                                      Spy defender_spy);
 
 #endif // ATTACKER_H
